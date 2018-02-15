4d223be3b487ff6034e2887f43d1dc83c06175d0
Emiel Muis <emiel_muis@hotmail.com>
2018-02-15 21:43:46 +0100

    @muise001

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,22 +1,64 @@
 console.log("global-scope");
 // IIFE
-(function(){
+(function() {
   "strict mode"
   console.log("local-scope");
   // Initialize App
   var app = {
-    init:function(){
+    search: function(){
+      document.querySelector("button").addEventListener("click", function(){
+        var search = document.querySelector("input").value;
+        document.querySelector("#results").innerHTML = ""
+        document.querySelector("#chosenOnes").innerHTML = ""
+        app.request(search)
+      })
+    },
+    request: function(search){
+      var request = new XMLHttpRequest();
+      request.open('GET', 'http://api.giphy.com/v1/gifs/search?q= '+ search +' &api_key=bMIPWUm5uWlqwJDmZPxDw4dKGzDZfGdd&limit=6', true);
+      //bMIPWUm5uWlqwJDmZPxDw4dKGzDZfGdd
+
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          console.log(data);
+          for (var i = 0; i < data.data.length; i++) {
+            if (data.data[i].username != "" ){
+              var rest = "<h3>Creator</h3>  <ol> <li>uploadert : "+data.data[i].user.username+"</li><li> user-url : "+data.data[i].user.profile_url+"</li> <li>Twitter : "+data.data[i].user.twitter+"</li> </ol></section>";
+            }
+            else {
+              var rest = ""
+            }
+            document.querySelector("#results").innerHTML += "<div><h3>"+data.data[i].title+"</h3><a href='#detail"+[i]+"'><img style='width:60%; min-width:8em;' src='" + data.data[i].images.original.url + "'></a></div>"
+            document.querySelector("#chosenOnes").innerHTML += "<section class='none' id='detail"+[i]+"'> <h1>"+data.data[i].title+"</h1> <img style='width: 80%; margin: 1em 10%;' src= "+data.data[i].images.original.url+" alt=> <ol>  <li>rating : "+data.data[i].rating+"</li>  <li>Geupload op : "+data.data[i].import_datetime+"</li> </ol>"+rest;
+          }
+
+        } else {
+          // We reached our target server, but it returned an error
+
+        }
+      };
+
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
+
+      request.send();
+    },
+    init: function() {
       routes.init()
       console.log("works, innit?");
+      this.search()
       // global app stuff
     }
   }
 
   // Handle routes & State
   var routes = {
-    init:function(){
+    init: function() {
       sections.toggle(window.location.hash)
-      window.addEventListener("hashchange",function(event){
+      window.addEventListener("hashchange", function(event) {
         var route = location.hash
         sections.toggle(route)
       })
@@ -25,8 +67,7 @@ console.log("global-scope");
 
   // Render / toggle sections
   var sections = {
-
-    toggle:function(route){
+    toggle: function(route) {
       if (route == "") {
         route = "#start"
       }
@@ -42,5 +83,5 @@ console.log("global-scope");
   }
 
   // Start App
-    app.init();
+  app.init();
 })()
